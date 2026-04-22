54236bf5e3097dfb17d3885676fe62f0ec81123e
hardware-numerics <gransalis16@gmail.com>
2026-04-22 17:52:14 +0200

    update

--- examples/demo.py
+++ examples/demo.py
@@ -5,12 +5,17 @@ import rk_stability_explorer as rk
 #print(rk_stability_explorer.__file__)
 
 
-methods = [
-    "euler", "rk4", "dopri5",
-    "optRK62", "ps36", "williamson",
-    "ssprk33", "kvarno", "radauIIA"
-]
+#methods = [
+#    "euler", "rk4", "dopri5",
+#    "optRK62", "ps36", "williamson",
+#    "ssprk33", "kvarno", "radauIIA"
+#]
 
+methods = [
+    "euler", "heun", "rk4", "dopri5",
+    "optRK62", "radauIIA", "kvarno", "williamson",
+    "ssprk33","ps36"
+]
 
 
 
@@ -18,7 +23,7 @@ X, Y = rk.make_grid(n=200)
 Z = X + 1j * Y
 
 results = rk.compute_results(methods, X, Y, Z)
-fig = rk.rkplot_multiple(results, cols=3, cmap = "twilight")
+fig = rk.rkplot_multiple(results, cols=5, cmap = "twilight")
 
-fig.savefig("DemoRK2.png",  dpi=200, bbox_inches = 'tight', pad_inches = 0)
+fig.savefig("DemoRK.png",  dpi=200, bbox_inches = 'tight', pad_inches = 0)
 plt.show()
