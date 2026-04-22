03cb6f8ba6523ee20cfedf391517d221b74fa4fc
hardware-numerics <gransalis16@gmail.com>
2026-04-22 21:20:12 +0200

    update

--- examples/demo.py
+++ examples/demo.py
@@ -20,5 +20,5 @@ Z = X + 1j * Y
 results = rk.compute_results(methods, X, Y, Z)
 fig = rk.rkplot_multiple(results, cols=5, cmap = "twilight")
 
-fig.savefig("assets/LinRK.png",  dpi=200, bbox_inches = 'tight', pad_inches = 0)
+fig.savefig("assets/DemoRK.png",  dpi=200, bbox_inches = 'tight', pad_inches = 0)
 plt.show()
